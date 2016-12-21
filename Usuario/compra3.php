d935658b7ad81cf459dcf676fb3fd030c7dc82cd
“razielmcr” <razielmcr1@hotmail.com>
2016-12-21 13:51:25 -0600

    Agregado decremento de Boletos disponibles al comprar

--- Usuario/compra3.php
+++ Usuario/compra3.php
@@ -11,20 +11,27 @@
 	$compra = $seccion . ",". $fila."," . $asiento;
 
 	$array = array($id_evento, $compra);
-	
+		
 	#Parte para verificar compra.
-	// $mysqli = new mysqli("localhost", "root", "pass", "eventos");
 	$result2 = $mysqli->query("SELECT Compra FROM asientos WHERE ID_evento = '$id_evento' and Compra = '$compra'");
 	if ($result2->num_rows == 0) {
 		$result	= insertar($conexion, $table, $array);
 		if ($result){
+			if($seccion == "p"){
+				$result3 = $mysqli->query("UPDATE eventos SET Premium = Premium -1 WHERE ID_evento = '$id_evento'");				
+			}else if($seccion == "e"){
+				$result3 = $mysqli->query("UPDATE eventos SET Estandar = Estandar -1 WHERE ID_evento = '$id_evento'");								
+			}else if($seccion == "d"){
+				$result3 = $mysqli->query("UPDATE eventos SET Discapacitados = Discapacitados -1 WHERE ID_evento = '$id_evento'");				
+				
+			}
 			echo "<p>Comprado </p>";
 		}
 		else{
 			echo "<p>no Comprado </p>";
 		}
 	}else{
-			echo "<p>EL boleto ya fue comprado por alguien mas, elija otro por favor. </p>";
+			echo "<p>El asiento elegido ya fue comprado por alguien mas, seleccione otro por favor. </p>";
 	
 	}
 
